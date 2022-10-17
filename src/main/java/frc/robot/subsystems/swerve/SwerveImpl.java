12f7d2995a9a770abed07c4025068d38f15a06b0
pooriaahmadi <pooriababak444@gmail.com>
2022-10-16 21:09:26 -0400

    turned into a subsystem

--- src/main/java/frc/robot/subsystems/swerve/SwerveImpl.java
+++ src/main/java/frc/robot/subsystems/swerve/SwerveImpl.java
@@ -2,9 +2,10 @@ package frc.robot.subsystems.swerve;
 
 import java.util.List;
 
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class SwerveImpl {
+public class SwerveImpl extends SubsystemBase {
 
 	/**
 	 * Swerve modules
@@ -13,5 +14,10 @@ public class SwerveImpl {
 
 	public SwerveImpl() {
 		this.swerveModules = SwerveModule.loadModules(Constants.swerveModules);
+	}
+
+	@Override
+	public void periodic() {
+
 	}
 }
