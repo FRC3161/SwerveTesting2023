e4c1c3b419a0a103f2027265793b1291dcceb204
pooriaahmadi <pooriababak444@gmail.com>
2022-10-16 21:09:12 -0400

    PID controller for relative motor

--- src/main/java/frc/robot/motors/RelativeMotor.java
+++ src/main/java/frc/robot/motors/RelativeMotor.java
@@ -2,14 +2,17 @@ package frc.robot.motors;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxPIDController;
 
 import frc.robot.constants.PID;
 
 public class RelativeMotor extends Motor {
     public RelativeEncoder encoder;
+    public SparkMaxPIDController PIDcontroller;
 
     public RelativeMotor(CANSparkMax controller, PID pid) {
         super(controller, pid);
         this.encoder = this.controller.getEncoder();
+        this.PIDcontroller = this.controller.getPIDController();
     }
 }
