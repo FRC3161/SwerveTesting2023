08524d267dd6b8154c58f1fd08e4e1bb8660234b
pooriaahmadi <pooriababak444@gmail.com>
2022-12-25 21:37:13 -0500

    vision tracking

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import java.util.List;
 import java.util.Map;
 
 import javax.naming.spi.DirStateFactory.Result;
@@ -7,15 +8,22 @@ import javax.naming.spi.DirStateFactory.Result;
 import com.ctre.phoenix.sensors.Pigeon2;
 import com.fasterxml.jackson.databind.deser.ValueInstantiator.Gettable;
 
+import org.photonvision.PhotonCamera;
+import org.photonvision.common.hardware.VisionLEDMode;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
@@ -30,9 +38,13 @@ public class Swerve extends SubsystemBase {
   public SwerveModule[] mSwerveMods;
   public Pigeon2 gyro;
   private Rotation2d orientationWhenReleased;
+  private Rotation2d orientationBeforeTarget;
   private double rotationControllerSpeed = 0.0;
   private final Field2d field = new Field2d();
   private final PIDController RobotRotationPID;
+  private final PIDController TargetRotationPID;
+  private final PIDController autoDrivePID;
+  private final PhotonCamera camera = new PhotonCamera("gloworm");
 
   private NetworkTableEntry yawEntry;
 
@@ -42,6 +54,11 @@ public class Swerve extends SubsystemBase {
     zeroGyro();
     this.RobotRotationPID = new PIDController(0.1, 0, 0.00005);
     this.RobotRotationPID.enableContinuousInput(-180, 180);
+
+    this.TargetRotationPID = new PIDController(6, 0, 0.05);
+    this.TargetRotationPID.enableContinuousInput(2, 2);
+
+    this.autoDrivePID = new PIDController(0.1, 0, 0);
     // this.RobotRotationPID.setTolerance(2.5);
 
     swerveOdometry = new SwerveDriveOdometry(Constants.Swerve.swerveKinematics, getYaw());
@@ -64,21 +81,47 @@ public class Swerve extends SubsystemBase {
     this.rotationControllerSpeed = rotation;
   }
 
-  public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
+  public double checkTargets(double missAlignment) {
+    PhotonPipelineResult results = this.camera.getLatestResult();
+    if (results.hasTargets()) {
+      List<PhotonTrackedTarget> targets = results.getTargets();
+      PhotonTrackedTarget target = targets.get(0);
+
+      int targetID = target.getFiducialId();
+      Transform3d bestCameraToTarget = target.getBestCameraToTarget();
+      SmartDashboard.putNumber("target id", targetID);
+      SmartDashboard.putNumber("target x", bestCameraToTarget.getX());
+      SmartDashboard.putNumber("target Y", bestCameraToTarget.getY());
+      this.orientationWhenReleased = this.getYaw();
+
+      return this.TargetRotationPID.calculate(-bestCameraToTarget.getY(), 0);
+    }
+
+    return missAlignment;
+  }
+
+  public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop,
+      boolean shoot) {
     this.rotationUpdate(rotation);
     double missalignment = MathUtil.clamp(
         this.RobotRotationPID.calculate(getYaw().getDegrees() % 360,
             this.orientationWhenReleased.getDegrees() % 360),
         -Constants.Swerve.maxSpeed, Constants.Swerve.maxSpeed);
 
+    if (shoot) {
+      // this.camera.setLED(VisionLEDMode.kOn);
+      missalignment = this.checkTargets(missalignment);
+    } else {
+      // this.camera.setLED(VisionLEDMode.kOff);
+    }
+
     SmartDashboard.putNumber("Missalignment speed PID", missalignment);
-    SmartDashboard.putBoolean("Ontarget", this.RobotRotationPID.atSetpoint());
 
     SwerveModuleState[] swerveModuleStates = Constants.Swerve.swerveKinematics.toSwerveModuleStates(
         fieldRelative
             ? ChassisSpeeds.fromFieldRelativeSpeeds(
                 translation.getX(), translation.getY(),
-                rotation - (this.RobotRotationPID.atSetpoint() ? 0 : missalignment),
+                rotation - missalignment,
                 getYaw())
             // translation.getX(), translation.getY(), rotation, getYaw())
             : new ChassisSpeeds(translation.getX(), translation.getY(), rotation));
@@ -143,8 +186,6 @@ public class Swerve extends SubsystemBase {
   public void periodic() {
     swerveOdometry.update(getYaw(), getStates());
     this.field.setRobotPose(swerveOdometry.getPoseMeters());
-
-    this.yawEntry.setNumber(this.getYaw().getDegrees());
 
     for (SwerveModule mod : mSwerveMods) {
       mod.updateDashboardValues();
